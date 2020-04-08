9a349379e4930e2a9eeff776110fcea8bbe3e61f
coderlkf <1593423396@qq.com>
2020-04-08 22:41:11 +0800

    init porj

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import mutations from '@/store/mutation'
+import actions from '@/store/action'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  mutations,
+  actions,
+  modules: {}
 })
